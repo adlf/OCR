9c836f0525b1f610453aac505f55080f729551bc
adolfo <adpinto95@gmail.com>
2016-10-10 22:54:50 +0100

    remove unnecessary weithing method

--- ACOCRMatlabScripts/train_associative_memory.m
+++ ACOCRMatlabScripts/train_associative_memory.m
@@ -1,7 +1,3 @@
-function result_AM = train_associative_memory(data, target, transpose)
-	if (transpose == 1)
-		result_AM = target * data';
-	else
-		result_AM = target * pinv(data);
-	end
+function result_AM = train_associative_memory(data, target)
+	result_AM = target * pinv(data);
 end
